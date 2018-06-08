af81a9fa51b92240af07a741468d97bef4222c86
MCMrARM <mcmrarm@gmail.com>
2018-06-08 16:41:05 +0200

    Generalize some basic EventBatch tests

--- test/file_event_batch.cpp
+++ test/file_event_batch.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <cll/file_event_batch.h>
+#include "event_batch.h"
 
 using namespace cll;
 
@@ -16,57 +17,9 @@ protected:
         remove(batch.getPath().c_str());
     }
 };
-class FileEventBatchWithDataTest : public FileEventBatchTest {
-protected:
-    static const int TEST_EVENT_COUNT = 128;
-
-    nlohmann::json GetJsonFor(int eventIndex);
-
-    void SetUp() override;
-};
-const int FileEventBatchWithDataTest::TEST_EVENT_COUNT;
-
-static std::vector<std::string> getMessagesInEventList(BatchedEventList* val) {
-    char const* ptr = val->getData();
-    std::vector<std::string> list;
-    if (val == nullptr)
-        return list;
-    while (true) {
-        char const* e = (char const*) memchr(ptr, '\n', val->getDataSize() - (ptr - val->getData()));
-        if (e == nullptr) {
-            if (ptr != val->getData() + val->getDataSize())
-                throw std::runtime_error("getMessagesInEventList: Has extra data after message");
-            break;
-        }
-        if (*e != '\n' || *(e - 1) != '\r')
-            throw std::runtime_error("getMessagesInEventList: Doesn't end with \\r\\n");
-        list.push_back(std::string(ptr, e - ptr - 1));
-        ptr = e + 1;
-    }
-    return list;
-}
 
 TEST_F(FileEventBatchTest, BasicTest) {
-    // Add event
-    nlohmann::json event = {{"test", "This is a test log entry"}};
-    ASSERT_FALSE(batch.hasEvents());
-    auto eventStr = event.dump();
-    ASSERT_TRUE(batch.addEvent(event));
-    batch.setFinalized();
-    ASSERT_TRUE(batch.hasEvents());
-    // Get the event back
-    auto upEv = batch.getEventsForUpload(1, 128); // this should return event + "\r\n"
-    ASSERT_NE(upEv, nullptr);
-    ASSERT_EQ(upEv->getDataSize(), eventStr.size() + 2);
-    ASSERT_TRUE(memcmp(eventStr.data(), upEv->getData(), eventStr.size()) == 0);
-    ASSERT_TRUE(memcmp(&upEv->getData()[eventStr.size()], "\r\n", 2) == 0);
-    // Check simple (full) deletion behaviour
-    batch.onEventsUploaded(*upEv);
-    ASSERT_FALSE(batch.hasEvents());
-    upEv = batch.getEventsForUpload(1, 128);
-    ASSERT_TRUE(upEv == nullptr || upEv->getDataSize() == 0);
-    // Make sure the file no longer exists (it was finalized, and all events should be removed)
-    ASSERT_FALSE(access(batch.getPath().c_str(), F_OK) == 0);
+    EventBatchTest::BasicTest(batch);
 }
 
 TEST(FileBackedEventBatchCustomTest, PersistenceTest) {
@@ -78,7 +31,7 @@ TEST(FileBackedEventBatchCustomTest, PersistenceTest) {
     }
     {
         FileEventBatch batch("test_data");
-        auto upEv = getMessagesInEventList(batch.getEventsForUpload(10, 512).get());
+        auto upEv = EventBatchTest::GetMessagesInEventList(batch.getEventsForUpload(10, 512).get());
         ASSERT_EQ(upEv.size(), 1);
         ASSERT_EQ(upEv[0], eventStr);
     }
@@ -90,44 +43,24 @@ TEST_F(FileEventBatchTest, NoAddingEventsToFinalized) {
     ASSERT_FALSE(batch.addEvent(nlohmann::json::object()));
 }
 
-void FileEventBatchWithDataTest::SetUp() {
-    for (int i = 0; i < TEST_EVENT_COUNT; i++) {
-        nlohmann::json event = GetJsonFor(i);
-        ASSERT_TRUE(batch.addEvent(event));
-    }
+TEST_F(FileEventBatchTest, FinalizedDeletion) {
+    nlohmann::json event = {{"test", "This is a test log entry"}};
+    ASSERT_TRUE(batch.addEvent(event));
+    batch.setFinalized();
+    auto upEv = batch.getEventsForUpload(1, 128);
+    ASSERT_NE(upEv, nullptr);
+    batch.onEventsUploaded(*upEv);
+    // Make sure the file no longer exists (it was finalized, and all events should be removed)
+    ASSERT_FALSE(access(batch.getPath().c_str(), F_OK) == 0);
 }
 
-nlohmann::json FileEventBatchWithDataTest::GetJsonFor(int eventIndex) {
-    return {{"test", "This is a test log entry #" + std::to_string(eventIndex)}};
+TEST_F(FileEventBatchTest, ReadIncremental) {
+    EventBatchTest::SetUpTestEvents(batch);
+    EventBatchTest::ReadIncremental(batch);
 }
-
-TEST_F(FileEventBatchWithDataTest, ReadIncremental) {
-    for (size_t i = 1; i < TEST_EVENT_COUNT; i++) {
-        auto val = batch.getEventsForUpload(i, i * 128);
-        ASSERT_GT(val->getDataSize(), 0) << "Iteration: " << i;
-        auto evs = getMessagesInEventList(val.get());
-        ASSERT_EQ(evs.size(), i);
-        for (size_t j = 0; j < i; j++) {
-            nlohmann::json expected = GetJsonFor(j);
-            ASSERT_EQ(expected.dump(), evs[j]);
-        }
-    }
-}
-TEST_F(FileEventBatchWithDataTest, ReadIncrementalWithRemoval) {
-    size_t maxCount = 1;
-    size_t gotEvents = 0;
-    while (gotEvents < TEST_EVENT_COUNT) {
-        auto val = batch.getEventsForUpload(maxCount, TEST_EVENT_COUNT * maxCount);
-        ASSERT_GT(val->getDataSize(), 0) << "Got count: " << gotEvents << "; Max count: " << maxCount;
-        for (std::string msg : getMessagesInEventList(val.get())) {
-            nlohmann::json expected = GetJsonFor((int) gotEvents);
-            ASSERT_EQ(expected.dump(), msg);
-            gotEvents++;
-            ASSERT_LE(gotEvents, TEST_EVENT_COUNT);
-        }
-        batch.onEventsUploaded(*val);
-        maxCount++;
-    }
+TEST_F(FileEventBatchTest, ReadIncrementalWithRemoval) {
+    EventBatchTest::SetUpTestEvents(batch);
+    EventBatchTest::ReadIncrementalWithRemoval(batch);
 }
 
 }
--- test/memory_event_batch.cpp
+++ test/memory_event_batch.cpp
@@ -1,26 +1,12 @@
 #include <cll/memory_event_batch.h>
 #include <gtest/gtest.h>
+#include "event_batch.h"
 
 using namespace cll;
 
 TEST(MemoryEventBatchTest, BasicTest) {
     MemoryEventBatch batch;
-    // Add event
-    nlohmann::json event = {{"test", "This is a test log entry"}};
-    ASSERT_FALSE(batch.hasEvents());
-    auto eventStr = event.dump();
-    ASSERT_TRUE(batch.addEvent(event));
-    ASSERT_TRUE(batch.hasEvents());
-    // Get the event back
-    auto upEv = batch.getEventsForUpload(1, 128); // this should return event + "\r\n"
-    ASSERT_EQ(upEv->getDataSize(), eventStr.size() + 2);
-    ASSERT_TRUE(memcmp(eventStr.data(), upEv->getData(), eventStr.size()) == 0);
-    ASSERT_TRUE(memcmp(&upEv->getData()[eventStr.size()], "\r\n", 2) == 0);
-    // Check deletion
-    batch.onEventsUploaded(*upEv);
-    ASSERT_FALSE(batch.hasEvents());
-    upEv = batch.getEventsForUpload(1, 128);
-    ASSERT_TRUE(upEv == nullptr || upEv->getDataSize() == 0);
+    EventBatchTest::BasicTest(batch);
 }
 
 TEST(MemoryEventBatchTest, OverflowTest) {
@@ -30,14 +16,14 @@ TEST(MemoryEventBatchTest, OverflowTest) {
     ASSERT_FALSE(batch.addEvent(event));
 }
 
-TEST(MemoryEventBatchTest, DeleteTest) {
-    MemoryEventBatch batch (5);
-    nlohmann::json event = {{"test", "This is a test log entry"}};
-    batch.addEvent(event);
-    nlohmann::json event2 = {{"test", "This is a test log entry #2"}};
-    batch.addEvent(event2);
-    batch.onEventsUploaded(*batch.getEventsForUpload(1, 1024));
-    auto ev2 = batch.getEventsForUpload(1, 1024);
-    ASSERT_NE(ev2, nullptr);
-    ASSERT_EQ(std::string(ev2->getData(), ev2->getDataSize()), event2.dump() + "\r\n");
+TEST(MemoryEventBatchTest, ReadIncremental) {
+    MemoryEventBatch batch (DEFAULT_TEST_EVENT_COUNT);
+    EventBatchTest::SetUpTestEvents(batch);
+    EventBatchTest::ReadIncremental(batch);
+}
+
+TEST(MemoryEventBatchTest, ReadIncrementalWithRemoval) {
+    MemoryEventBatch batch (DEFAULT_TEST_EVENT_COUNT);
+    EventBatchTest::SetUpTestEvents(batch);
+    EventBatchTest::ReadIncrementalWithRemoval(batch);
 }
