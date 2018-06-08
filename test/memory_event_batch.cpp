e7abc70ebf72b781d5f6144151d001f36320e268
MCMrARM <mcmrarm@gmail.com>
2018-06-08 15:40:48 +0200

    Add MemoryEventBatchTest.DeleteTest

--- test/memory_event_batch.cpp
+++ test/memory_event_batch.cpp
@@ -22,3 +22,22 @@ TEST(MemoryEventBatchTest, BasicTest) {
     upEv = batch.getEventsForUpload(1, 128);
     ASSERT_TRUE(upEv == nullptr || upEv->getDataSize() == 0);
 }
+
+TEST(MemoryEventBatchTest, OverflowTest) {
+    MemoryEventBatch batch (1);
+    nlohmann::json event = {{"test", "This is a test log entry"}};
+    ASSERT_TRUE(batch.addEvent(event));
+    ASSERT_FALSE(batch.addEvent(event));
+}
+
+TEST(MemoryEventBatchTest, DeleteTest) {
+    MemoryEventBatch batch (5);
+    nlohmann::json event = {{"test", "This is a test log entry"}};
+    batch.addEvent(event);
+    nlohmann::json event2 = {{"test", "This is a test log entry #2"}};
+    batch.addEvent(event2);
+    batch.onEventsUploaded(*batch.getEventsForUpload(1, 1024));
+    auto ev2 = batch.getEventsForUpload(1, 1024);
+    ASSERT_NE(ev2, nullptr);
+    ASSERT_EQ(std::string(ev2->getData(), ev2->getDataSize()), event2.dump() + "\r\n");
+}
