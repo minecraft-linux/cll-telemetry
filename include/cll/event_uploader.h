10f03bc90e33b5736fe6e37dee6578b6210a32ed
MCMrARM <mcmrarm@gmail.com>
2018-06-09 21:58:37 +0200

    EventUploadStep: docs, fix return type for auth failure

--- include/cll/event_uploader.h
+++ include/cll/event_uploader.h
@@ -11,8 +11,18 @@ namespace cll {
 class EventUploadStep {
 
 public:
+    /**
+     * This function is called before a request is made. This is the place to add custom headers (eg. authentication).
+     * @param request the request info
+     */
     virtual void onRequest(EventUploadRequest const& request) {}
-    virtual bool onAuthenticationFailed() {}
+
+    /**
+     * This function is called when a request has failed with a 401 status code. You can re-request any new tokens here,
+     * and if you think the request should be retried, return true.
+     * @return if the request should be retried.
+     */
+    virtual bool onAuthenticationFailed() { return false; }
 
 };
 
