6a22e465331680b35abcd9f122aae9b69a476212
MCMrARM <mcmrarm@gmail.com>
2018-06-09 22:00:27 +0200

    Move EventUploadStep and EventUploadRequest into a new header

---
+++ include/cll/event_upload_step.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "event.h"
+
+namespace cll {
+
+struct EventUploadRequest {
+    std::vector<std::pair<std::string, std::string>> headers;
+};
+
+class EventUploadStep {
+
+public:
+    /**
+     * This function is called before a request is made. This is the place to add custom headers (eg. authentication).
+     * @param request the request info
+     */
+    virtual void onRequest(EventUploadRequest const& request) {}
+
+    /**
+     * This function is called when a request has failed with a 401 status code. You can re-request any new tokens here,
+     * and if you think the request should be retried, return true.
+     * @return if the request should be retried.
+     */
+    virtual bool onAuthenticationFailed() { return false; }
+
+};
+
+}
--- include/cll/event_uploader.h
+++ include/cll/event_uploader.h
@@ -3,28 +3,10 @@
 #include <memory>
 #include <vector>
 #include "event.h"
-#include "event_upload_request.h"
+#include "event_upload_step.h"
 #include "event_batch.h"
 
 namespace cll {
-
-class EventUploadStep {
-
-public:
-    /**
-     * This function is called before a request is made. This is the place to add custom headers (eg. authentication).
-     * @param request the request info
-     */
-    virtual void onRequest(EventUploadRequest const& request) {}
-
-    /**
-     * This function is called when a request has failed with a 401 status code. You can re-request any new tokens here,
-     * and if you think the request should be retried, return true.
-     * @return if the request should be retried.
-     */
-    virtual bool onAuthenticationFailed() { return false; }
-
-};
 
 struct EventUploadStatus {
 
